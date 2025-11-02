b85602eec6761d68eb49ec39ecb278855255fbc8
Ryan Wang <i@ryanc.cc>
2025-11-02 22:00:39 +0800

    Migrate console build from Rspack to Rsbuild

---
+++ console/rsbuild.config.mjs
@@ -0,0 +1,21 @@
+import { rsbuildConfig } from "@halo-dev/ui-plugin-bundler-kit";
+import { pluginSass } from "@rsbuild/plugin-sass";
+
+const OUT_DIR_PROD = "../src/main/resources/console";
+const OUT_DIR_DEV = "../build/resources/main/console";
+
+export default rsbuildConfig({
+  rsbuild: ({ envMode }) => {
+    const isProduction = envMode === "production";
+    const outDir = isProduction ? OUT_DIR_PROD : OUT_DIR_DEV;
+
+    return {
+      output: {
+        distPath: {
+          root: outDir,
+        },
+      },
+      plugins: [pluginSass()],
+    };
+  },
+});
